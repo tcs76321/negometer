259b10e59c240dac6ccbcc8a27baa41ad44f252a
Trevor Stahl <trevorclarkstahl@gmail.com>
2021-12-14 13:59:27 -0700

    done with front page rough draft, hooking up links

--- mainfunc/urls.py
+++ mainfunc/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('abc', views.index, name='index'),
+    path('cbs', views.index, name='index'),
+    path('cnn', views.index, name='index'),
+    path('fox', views.index, name='index'),
+    path('nbc', views.index, name='index'),
+    path('about', views.index, name='index'),
+    path('donate', views.index, name='index'),
+    path('contact', views.index, name='index'),
 ]
